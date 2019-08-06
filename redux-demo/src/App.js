2ec2dcee4274dc57e81786eeca45062a02074fb9
Anna Langner <annalangner06@gmail.com>
2019-08-06 11:43:50 +0200

    Adding an import from ComentsListContainer.js

--- redux-demo/src/App.js
+++ redux-demo/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import CommentList from "./CommentsListContainer";
 
 const App = () => {
   return (
     <div className='App'>
-      Here's a list of comments!
+      <CommentList />
     </div>
   );
 };
